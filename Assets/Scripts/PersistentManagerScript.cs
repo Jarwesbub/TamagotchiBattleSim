9bba9fa0d5221013b5faca180ae42035fbd5304f
Jarno <jarwes90@gmail.com>
2020-04-28 13:02:51 +0300

    Base update

---
+++ Assets/Scripts/PersistentManagerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PersistentManagerScript : MonoBehaviour
+{
+    public static PersistentManagerScript Instance { get; private set; }
+
+    public int Str;
+    public int Agi;
+    public int Int;
+
+    private void Awake()
+    {
+        if (Instance == null) //if instance contains no data (when game starts) -> dont destroy
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+        }
+        else
+        {
+            Destroy(gameObject); //if instance already contains data -> destroy duplicant (dont create again)
+        }
+
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public Text StrTxt;
+    public Text AgiTxt;
+    public Text IntTxt;
+
+
+    private void Start()
+    {
+        StrTxt.text = PersistentManagerScript.Instance.Str.ToString();
+        StrTxt.text = "Str  " + StrTxt.text;
+
+        AgiTxt.text = PersistentManagerScript.Instance.Agi.ToString();
+        AgiTxt.text = "Agi  " + AgiTxt.text;
+
+        IntTxt.text = PersistentManagerScript.Instance.Int.ToString();
+        IntTxt.text = "Int  " + IntTxt.text;
+
+    }
+
+    public void GoToTamaMenuScene()
+    {
+        SceneManager.LoadScene("TamaMenuScene");
+        PersistentManagerScript.Instance.Str++; //test add 1
+
+    }
+    public void GoToWorld1Scene()
+    {
+        SceneManager.LoadScene("World1Scene");
+        PersistentManagerScript.Instance.Str++; //test add 1
+
+    }
+}
